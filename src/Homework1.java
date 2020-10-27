231377a4bffb1977c02f3478cf9a7e8bb06d0aa6
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-10-27 08:01:11 +0400

    homework

---
+++ src/Homework1.java
@@ -0,0 +1,22 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        byte byteNumber = 40;
+        System.out.println(byteNumber);
+        short shortNumber = 400;
+        System.out.println(shortNumber);
+        int intNumber = 4000000;
+        System.out.println(intNumber);
+        long longNumber = 40000000000000L;
+        System.out.println(longNumber);
+        float floatNumber = 45.88f;
+        System.out.println(floatNumber);
+        double doubleNumber = 88.99999;
+        System.out.println(doubleNumber);
+        char symbal = 'k';
+        System.out.println(symbal);
+        boolean flag = true;
+        System.out.println(flag);
+        String text = "Hello World!!";
+        System.out.println(text);
+    }
+}
